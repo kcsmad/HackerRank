609e63e3540e79eaffc384118535b4f218504cd8
Kevin Maduro <nivekzin@gmail.com>
2018-08-05 02:23:33 -0300

    First SaveThePrincess Project for AI Challenge

--- SaveThePrincess/Program.cs
+++ SaveThePrincess/Program.cs
@@ -8,8 +8,77 @@ namespace SaveThePrincess
 {
     class Program
     {
-        static void Main(string[] args)
+        static void displayPathtoPrincess(int n, String[] grid)
         {
+            int botX = 0;
+            int botY = 0;
+            int princessX = 0;
+            int princessY = 0;
+
+            for (int line = 0; line < n; line++)
+            {
+                if (grid[line].Contains("m"))
+                {
+                    botX = line;
+                    botY = grid[line].IndexOf('m');
+                }
+                else if (grid[line].Contains("p"))
+                {
+                    princessX = line;
+                    princessY = grid[line].IndexOf('p');
+                }
+            }
+
+            int distanceX = princessX - botX;
+            int distanceY = princessY - botY;
+
+            for (int walk = 0; walk < Math.Abs(distanceX); walk++)
+            {
+                if (botX < princessX)
+                {
+                    Console.WriteLine("DOWN");
+                }
+                else
+                {
+                    Console.WriteLine("UP");
+                }
+            }
+
+            for (int walk = 0; walk < Math.Abs(distanceY); walk++)
+            {
+                if (botY < princessY)
+                {
+                    Console.WriteLine("RIGHT");
+                }
+                else
+                {
+                    Console.WriteLine("LEFT");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Main(String[] args)
+        {
+            int m;
+
+            m = 5;
+
+            String[] grid = new String[5];
+
+            grid[0] = "-----";
+            grid[1] = "-----";
+            grid[2] = "--m--";
+            grid[3] = "-----";
+            grid[4] = "--p--";
+
+            //for (int i = 0; i < m; i++)
+            //{
+            //    grid[i] = Console.ReadLine();
+            //}
+
+            displayPathtoPrincess(m, grid);
         }
     }
 }
